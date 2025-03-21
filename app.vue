78280e0a6eba4500ced5f526fc99137bce909382
FLY123 <akfakf0509@naver.com>
2025-03-21 10:32:49 +0900

    chore: init clarity, generate dist

--- app.vue
+++ app.vue
@@ -1,3 +1,11 @@
+<script setup lang="ts">
+import Clarity from "@microsoft/clarity";
+
+onMounted(() => {
+  Clarity.init("qr36jhd7tr");
+});
+</script>
+
 <template>
   <div>
     <NuxtRouteAnnouncer />
